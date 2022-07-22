b4d1d69bf24db13c55ee707c5d1f53f05d86a66c
Gregorius Albert <galbert.djaja@gmail.com>
2022-07-22 20:35:16 +0700

    Create new palette view storyboard layout

--- Colorio/Scenes/Palette Collection/Create Palette/CreateNewPaletteView.swift
+++ Colorio/Scenes/Palette Collection/Create Palette/CreateNewPaletteView.swift
@@ -7,8 +7,35 @@
 
 import UIKit
 
+protocol CreateNewPaletteDelegate {
+    func pushPickerViewController()
+}
+
 class CreateNewPaletteView: UIView {
     
+    @IBOutlet weak var singleCard: UIView!
+    @IBOutlet weak var colorBox: UIView!
+    @IBOutlet weak var colorName: UILabel!
+    @IBOutlet weak var colorHexValue: UILabel!
+    @IBOutlet weak var colorRgbValue: UILabel!
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    var delegate: CreateNewPaletteDelegate!
+    
+    func setup(viewController: CreateNewPaletteViewController) {
+        delegate = viewController
+        tableView.delegate = viewController
+        tableView.dataSource = viewController
+        tableView.register(UINib(nibName: "SingleColorTableViewCell", bundle: nil), forCellReuseIdentifier: "singleColorCell")
+        
+        colorBox.layer.cornerRadius = 10
+        singleCard.layer.cornerRadius = 10
+        
+    }
+    
+    @IBAction func pickerButton(_ sender: Any) {
+        delegate.pushPickerViewController()
+    }
     
 }
--- Colorio/Scenes/Palette Collection/Create Palette/CreateNewPaletteViewController.swift
+++ Colorio/Scenes/Palette Collection/Create Palette/CreateNewPaletteViewController.swift
@@ -9,16 +9,62 @@ import UIKit
 
 class CreateNewPaletteViewController: UIViewController {
 
+    
+    @IBOutlet var createNewPaletteView: CreateNewPaletteView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        createNewPaletteView.setup(viewController: self)
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         title = "New Palette"
         navigationItem.largeTitleDisplayMode = .never
-
     }
 
+}
+
+extension CreateNewPaletteViewController: CreateNewPaletteDelegate {
+    func pushPickerViewController() {
+        // TODO: Push ColorPickerVC
+    }
+}
+
+extension CreateNewPaletteViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "singleColorCell") as! SingleColorTableViewCell
+        
+//        let color = colorArr[indexPath.section]
+//        
+//        cell.colorHex.text = "\(color.hex)"
+//        cell.colorName.text = "\(color.name)"
+//        cell.color.backgroundColor = UIColor(
+//            displayP3Red: CGFloat(color.red)/255.0,
+//            green: CGFloat(color.green)/255.0,
+//            blue: CGFloat(color.blue)/255.0,
+//            alpha: 1.0
+//        )
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+//        return colorArr.count
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+//        UIPasteboard.general.string = colorArr[indexPath.section].hex
+        showToast(message: "Hex copied to clipboard", seconds: 1)
+    }
 }
