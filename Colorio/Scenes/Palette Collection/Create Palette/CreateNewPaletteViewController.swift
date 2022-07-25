57c515e7a20bff58c6b0a1097b3e2be86a08a596
Gregorius Albert <galbert.djaja@gmail.com>
2022-07-25 18:57:24 +0700

    Load palette from single color ok

--- Colorio/Scenes/Palette Collection/Create Palette/CreateNewPaletteViewController.swift
+++ Colorio/Scenes/Palette Collection/Create Palette/CreateNewPaletteViewController.swift
@@ -13,7 +13,9 @@ class CreateNewPaletteViewController: UIViewController {
     @IBOutlet var createNewPaletteView: CreateNewPaletteView!
     
     var rgbString:String = ""
-    
+    var globalColor:UIColor?
+    var colorArr = [Color]()
+    var rgbCollection = [[Int]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +27,17 @@ class CreateNewPaletteViewController: UIViewController {
         navigationItem.largeTitleDisplayMode = .never
     }
     
-    func getSingleColorValue(rgbString:String) -> Void {
+    func emptyLoadingContent() -> Void {
+        colorArr.removeAll()
+        createNewPaletteView.colorName.text = "Loading..."
+        createNewPaletteView.colorHexValue.text = "Loading..."
+        createNewPaletteView.colorRgbValue.text = "Loading..."
+        createNewPaletteView.tableView.reloadData()
+    }
+    
+    func getSingleColorValueAndPalette(rgbString:String) -> Void {
+        
+        emptyLoadingContent()
         
         let url = URL(string: "\(Constants.THECOLORAPI_RGB_URL)\(rgbString)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)
         var request = URLRequest(url: url!)
@@ -44,10 +56,83 @@ class CreateNewPaletteViewController: UIViewController {
             DispatchQueue.main.sync {
                 self.createNewPaletteView.colorName.text = nameValue
                 self.createNewPaletteView.colorHexValue.text = hexValue
+                self.createNewPaletteView.colorRgbValue.text = "RGB(\(rgbString))"
+                self.getPalette()
             }
+            
         }.resume()
         
     }
+    
+    func getPalette() -> Void {
+            
+        let uIntColor = globalColor?.rgb
+        let red = uIntColor![0]
+        let green = uIntColor![1]
+        let blue = uIntColor![2]
+        
+        let url = URL(string: "\(Constants.COLORMIND_API_URL)")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+        
+        request.httpBody = try! JSONSerialization.data(withJSONObject: [
+            "input": [[red, green, blue]],
+            "model": "default"
+        ])
+        
+//        print("\n\n\( String(decoding: request.httpBody!, as: UTF8.self) )\n\n")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            let json = try! JSONSerialization.jsonObject(with: data!) as! [String:Any]
+            
+            let result = json["result"] as! [[Int]]
+            
+            DispatchQueue.main.sync {
+                self.rgbCollection = result
+                self.loadToTableView(index: 0)
+            }
+            
+        }.resume()
+        
+    }
+    
+    
+    func loadToTableView(index: Int) -> Void {
+        
+        guard index < 5 else { return }
+        
+        var param = "\(rgbCollection[index])"
+        param = param.replacingOccurrences(of: "[", with: "(").replacingOccurrences(of: "]", with: ")")
+        print("Param = \(param)")
+        
+        let url = URL(string: "\(Constants.THECOLORAPI_RGB_URL)\(param)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)
+        var request = URLRequest(url: url!)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            let json = try! JSONSerialization.jsonObject(with: data!) as! [String:Any]
+            
+            let name = json["name"] as! [String:Any]
+            let nameValue = name["value"] as! String
+            
+            let hex = json["hex"] as! [String:Any]
+            let hexValue = hex["value"] as! String
+            
+            DispatchQueue.main.sync {
+                let color = Color(name: nameValue, hex: hexValue, red: self.rgbCollection[index][0], green: self.rgbCollection[index][1], blue: self.rgbCollection[index][2])
+                self.colorArr.append(color)
+                self.createNewPaletteView.tableView.insertSections(IndexSet(integer: index), with: .fade)
+            }
+            
+            self.loadToTableView(index: index+1)
+            
+        }.resume()
+        
+    }
+    
+    
 
 }
 
@@ -64,33 +149,17 @@ extension CreateNewPaletteViewController: UIColorPickerViewControllerDelegate, C
     
     func getColorValueOnDismiss() {
         let color = createNewPaletteView.pickerVC.selectedColor
+        globalColor = color
         
         rgbString = "\(color.rgb)"
             .replacingOccurrences(of: "[", with: "(")
             .replacingOccurrences(of: "]", with: ")")
         
-        createNewPaletteView.colorRgbValue.text = "RGB\(rgbString)"
+//        createNewPaletteView.colorRgbValue.text = "RGB\(rgbString)"
         createNewPaletteView.colorBox.backgroundColor = color
-        getSingleColorValue(rgbString: rgbString)
+        getSingleColorValueAndPalette(rgbString: rgbString)
 
     }
-    
-    func colorPickerViewControllerDidFinish(_ viewController: UIColorPickerViewController) {
-        getSingleColorValue(rgbString: rgbString)
-    }
-    
-    func colorPickerViewControllerDidSelectColor(_ viewController: UIColorPickerViewController) {
-        let color = viewController.selectedColor
-        rgbString = "\(color.rgb)"
-            .replacingOccurrences(of: "[", with: "(")
-            .replacingOccurrences(of: "]", with: ")")
-        
-        createNewPaletteView.colorRgbValue.text = "RGB\(rgbString)"
-        createNewPaletteView.colorBox.backgroundColor = color
-
-        // TODO: Get color palette from colormind.io
-    }
-    
     
 }
 
@@ -98,19 +167,16 @@ extension CreateNewPaletteViewController: UITableViewDelegate, UITableViewDataSo
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "singleColorCell") as! SingleColorTableViewCell
+        let color = colorArr[indexPath.section]
         
-        //TODO: Configure cell items
-        
-//        let color = colorArr[indexPath.section]
-//        
-//        cell.colorHex.text = "\(color.hex)"
-//        cell.colorName.text = "\(color.name)"
-//        cell.color.backgroundColor = UIColor(
-//            displayP3Red: CGFloat(color.red)/255.0,
-//            green: CGFloat(color.green)/255.0,
-//            blue: CGFloat(color.blue)/255.0,
-//            alpha: 1.0
-//        )
+        cell.colorHex.text = "\(color.hex)"
+        cell.colorName.text = "\(color.name)"
+        cell.color.backgroundColor = UIColor(
+            displayP3Red: CGFloat(color.red)/255.0,
+            green: CGFloat(color.green)/255.0,
+            blue: CGFloat(color.blue)/255.0,
+            alpha: 1.0
+        )
         
         return cell
     }
@@ -120,8 +186,7 @@ extension CreateNewPaletteViewController: UITableViewDelegate, UITableViewDataSo
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-//        return colorArr.count
-        return 5
+        return colorArr.count
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
@@ -130,8 +195,7 @@ extension CreateNewPaletteViewController: UITableViewDelegate, UITableViewDataSo
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        // TODO: Implement pasteboard
-//        UIPasteboard.general.string = colorArr[indexPath.section].hex
+        UIPasteboard.general.string = colorArr[indexPath.section].hex
         showToast(message: "Hex copied to clipboard", seconds: 1)
     }
 }
