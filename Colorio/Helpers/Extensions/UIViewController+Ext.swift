95a469f0e8053681fb4d385478796cc89aabcb64
Gregorius Albert <galbert.djaja@gmail.com>
2022-07-28 10:47:03 +0700

    Fix toast

--- Colorio/Helpers/Extensions/UIViewController+Ext.swift
+++ Colorio/Helpers/Extensions/UIViewController+Ext.swift
@@ -12,7 +12,7 @@ extension UIViewController {
     func showToast(message: String, seconds: Double) {
         let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
         alert.view.backgroundColor = UIColor.black
-        alert.view.alpha = 0.6
+        alert.view.alpha = 0.2
         alert.view.layer.cornerRadius = 15
         present(alert, animated: true)
         
